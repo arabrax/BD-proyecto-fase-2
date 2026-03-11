9a565a890fc0c7e14bdc11fcc6dbc5366b9bb99f
Barbie RM <barbara.1297@gmail.com>
2026-03-11 11:39:12 -0400

    Funciones (fn_)

--- Programmability.sql
+++ Programmability.sql
@@ -31,12 +31,94 @@
 -- 1. fn_calcular_impuesto(monto): Recibe un monto decimal. Devuelve el impuesto calculado (asumir 16% o el valor que prefieran,
 -- pero debe ser parametrizable o constante).
 
+CREATE OR ALTER FUNCTION fn_calcular_impuesto
+ (
+  @monto DECIMAL(10,2)
+ )
+RETURNS NVARCHAR
+AS
+$$
+DECLARE
+    impuesto FLOAT;
+    impuesto_porcentaje FLOAT FOR $16;
+BEGIN
+
+    impuesto := @monto * (impuesto_porcentaje / 100);
+
+    RETURN CAST(impuesto);
+END;
+$$ LANGUAGE plpgsql;
+
 -- 2. fn_clasificar_ingreso(monto): Devuelve un NVARCHAR. Lógica: Si el monto > $1000 retorna 'Diamante', entre $500 y $1000 'Oro',
 -- y menor a $500 'Plata'.
+
+CREATE FUNCTION fn_clasificar_ingreso
+ (
+  @monto DECIMAL(10,2)
+ )
+RETURNS NVARCHAR
+AS
+DECLARE
+    string clasificacion;
+BEGIN
+    CASE
+        WHEN @monto > 1000 THEN clasificacion := 'Diamante'
+        WHEN @monto > 500 THEN clasificacion := 'Oro'
+        ELSE clasificacion := 'Plata'
+    END;
+
+    RETURN clasificacion;
+END;
+$$ LANGUAGE plpgsql;
 
 -- 3. fn_calcular_reputacion(idCreador): Devuelve un DECIMAL (0-100).
 --     ○ Fórmula sugerida: (Total Suscriptores * 0.5) + (Total Reacciones Último Mes * 0.1) + (Antigüedad Meses * 2).
 --     Tope máximo de 100 puntos.
+
+CREATE FUNCTION fn_calcular_reputacion
+ (
+  @idCreador INT
+ )
+RETURNS NVARCHAR
+AS $$
+DECLARE
+    suscriptorTotal INT;
+    reaccionesTotalUltMes INT;
+    antiguedadMeses INT;
+    reputacion INT;
+BEGIN
+
+    -- Total Suscriptores
+    SELECT COUNT(s.idUsuario) -- count o sum?
+    INTO suscriptorTotal
+    FROM Creador AS c
+    LEFT JOIN NivelSuscripcion AS ns ON c.idUsuario = ns.idCreador
+    LEFT JOIN Suscripcion AS s ON ns.id = s.idNivel
+    WHERE c.idUsuario = @idCreador;
+
+    -- Total Reacciones Último Mes
+    SELECT COUNT(urp.idTipoReaccion)
+    INTO reaccionesTotalUltMes
+    FROM Creador AS c
+    LEFT JOIN Publicacion AS p ON c.idUsuario = p.idCreador
+    LEFT JOIN UsuarioReaccionPublicacion AS urp ON p.id = urp.idPublicacion
+    WHERE c.idUsuario = @idCreador AND urp.fecha_reaccion > DATEDIFF(CURRENT_DATE,MONTH,-1);
+
+    -- Antigüedad Meses
+    SELECT DATEDIFF(u.fecha_registro, MONTH, CURRENT_DATE)
+    INTO antiguedadMeses
+    FROM Creador as c
+    LEFT JOIN Usuario as u ON c.idUsuario = u.id
+    WHERE c.idUsuario = @idCreador;
+
+    reputacion := (suscriptorTotal * 0,5) + (reaccionesTotalUltMes * 0,1) + (antiguedadMeses * 2);
+    
+    IF(reputacion > 100)
+        reputacion = 100;
+
+    RETURN reputacion;
+END;
+$$ LANGUAGE plpgsql;
 
 -- C. Triggers
 
